597bd7c8a99efa733078669af2a2b746c2d5d6a8
muhamad-kurniawan <44334305+muhamad-kurniawan@users.noreply.github.com>
2025-01-09 17:40:09 +0900

    Update madani.py

--- model/madani.py
+++ model/madani.py
@@ -447,7 +447,7 @@ class EncoderMoE(nn.Module):
 class Madani(nn.Module):
     def __init__(self,
                  out_dims=3,
-                 d_model=256,
+                 d_model=512,
                  N=3,
                  heads=4,
                  compute_device=None,
