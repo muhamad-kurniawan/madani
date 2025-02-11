bdeb8974a6a853e1ceea09a4ee0df637fb902c40
muhamad-kurniawan <44334305+muhamad-kurniawan@users.noreply.github.com>
2025-02-11 14:14:52 +0900

    Update kingcrab_rkconauto.py

--- model/kingcrab_rkconauto.py
+++ model/kingcrab_rkconauto.py
@@ -60,7 +60,7 @@ class Embedder(nn.Module):
 
         elem_dir = 'madani/data/element_properties'
         mat2vec = f'{elem_dir}/mat2vec.csv'  # element embedding
-        # mat2vec = f'{elem_dir}/oliynyk.csv'
+        mat2vec = f'{elem_dir}/oliynyk.csv'
         cbfv = pd.read_csv(mat2vec, index_col=0).values
         feat_size = cbfv.shape[-1]
         self.fc_mat2vec = nn.Linear(feat_size, d_model).to(self.compute_device)
