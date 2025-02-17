c386e058e47e4cc15826533ec6ec7412cb127052
muhamad-kurniawan <44334305+muhamad-kurniawan@users.noreply.github.com>
2025-02-17 14:07:46 +0900

    Update kingcrab_rkconauto_v0_9_3.py

--- model/kingcrab_rkconauto_v0_9_3.py
+++ model/kingcrab_rkconauto_v0_9_3.py
@@ -115,7 +115,7 @@ class Embedder(nn.Module):
         mat2vec = f'{elem_dir}/mat2vec.csv'  # element embedding file
         # mat2vec = f'{elem_dir}/onehot.csv'
         mat2vec = f'{elem_dir}/magpie.csv'
-        # mat2vec = f'{elem_dir}/jarvis.csv'
+        mat2vec = f'{elem_dir}/jarvis.csv'
         # mat2vec = f'{elem_dir}/oliynyk.csv'
         # mat2vec = f'{elem_dir}/oliynyk_2024.csv'
         # mat2vec = f'{elem_dir}/data_test.csv'
