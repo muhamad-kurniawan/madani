716d55e50c6047535d0ac374b18baf109843708f
muhamad-kurniawan <44334305+muhamad-kurniawan@users.noreply.github.com>
2025-02-24 10:48:00 +0900

    Update trainer_concrete.py

--- model/trainer_concrete.py
+++ model/trainer_concrete.py
@@ -165,7 +165,7 @@ class ModelTrainer:
                 loss = self.criterion(prediction.reshape(-1),
                                       uncertainty.reshape(-1),
                                       y.reshape(-1))
-                lambda_entropy = 0.0001
+                lambda_entropy = 0.00005
                 penalty = lambda_entropy * self.model.encoder.embed.feature_selector.entropy_penalty
 
                 loss = loss + penalty
