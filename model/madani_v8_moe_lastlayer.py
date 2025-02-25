afc05d6e29f7642611e91fa459b5a7f8ff1e9680
muhamad-kurniawan <44334305+muhamad-kurniawan@users.noreply.github.com>
2025-02-25 17:59:54 +0900

    Update madani_v8_moe_lastlayer.py

--- model/madani_v8_moe_lastlayer.py
+++ model/madani_v8_moe_lastlayer.py
@@ -427,7 +427,7 @@ class EncoderMoE(nn.Module):
             self.transformer_encoder = CustomTransformerEncoderMixedLastMoE(
                 d_model=self.d_model,
                 nhead=self.heads,
-                dim_feedforward=1796,
+                dim_feedforward=1024,
                 dropout=0.1,
                 layer_norm_eps=1e-5,
                 num_experts=num_experts,
