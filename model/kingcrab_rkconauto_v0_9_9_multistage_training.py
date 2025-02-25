731d69ce9698d4151f1d0f92ee9ff3586cda6a04
muhamad-kurniawan <44334305+muhamad-kurniawan@users.noreply.github.com>
2025-02-25 16:37:40 +0900

    Update kingcrab_rkconauto_v0_9_9_multistage_training.py

--- model/kingcrab_rkconauto_v0_9_9_multistage_training.py
+++ model/kingcrab_rkconauto_v0_9_9_multistage_training.py
@@ -152,7 +152,7 @@ class Embedder(nn.Module):
         mat2vec = f'{elem_dir}/mat2vec.csv'  # element embedding file
         mat2vec = f'{elem_dir}/oliynyk.csv'
         mat2vec = f'{elem_dir}/oliynyk_2024.csv'
-        # mat2vec = f'{elem_dir}/magpie.csv'
+        mat2vec = f'{elem_dir}/magpie.csv'
         # mat2vec = f'{elem_dir}/jarvis.csv'
 
         # Load the mat2vec CSV file into a pandas DataFrame.
